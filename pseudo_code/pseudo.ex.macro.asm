180b6e6e05cc422185dfb261fb951f8a13cbfe88
Pararaum <pararaum@gmail.com>
2018-04-20 23:34:23 +0200

    Set and clear bits.

--- pseudo_code/pseudo.ex.macro.asm
+++ pseudo_code/pseudo.ex.macro.asm
@@ -35,7 +35,7 @@ clearscreen:	lda #$41
 	P_branchNZ $fe, .l2
 	rts
 
-lfsr:	P_transfer $fe, $fc
+lfsrO:	P_transfer $fe, $fc
 	P_transfer $fe, $60
 	P_loadi $62, BITMAPPTR
 .l2:
@@ -63,10 +63,39 @@ lfsr:	P_transfer $fe, $fc
 	P_branchNZ $fe, .l2
 	lda #$ff		;Zero is never reached therefore hardcoded here.
 	sta BITMAPPTR
-	inc $d020
-	nop
 	rts
 .bitlist: .byte $01, $02, $04, $08, $10, $20, $40, $80
+
+lfsrA:	P_transfer $fe, $fc
+	P_transfer $fe, $60
+	P_loadi $62, BITMAPPTR
+.l2:
+	ldx $fc
+	P_shiftr $fc
+	txa
+	and #$01
+	beq .l1
+	P_eor $60, $fc
+.l1:
+	P_transfer $fc, $fe
+	P_shiftr $fe
+	P_shiftr $fe
+	P_shiftr $fe
+	P_add $62, $fe
+	lda $fc
+	and #$07
+	tax
+	ldy #0
+	lda ($fe),y
+	and .bitlist,x
+	sta ($fe),y
+	P_transfer $fc, $fe
+	P_sub $60, $fe
+	P_branchNZ $fe, .l2
+	lda #$00		;Zero is never reached therefore hardcoded here.
+	sta BITMAPPTR
+	rts
+.bitlist: .byte ~$01, ~$02, ~$04, ~$08, ~$10, ~$20, ~$40, ~$80
 
 main:	sei
 	lda #$35   ;Turn off the BASIC and KERNAL rom.
@@ -81,13 +110,17 @@ main:	sei
 	sta $0400
 	nop
 	P_loadi $fe, $84BE	;see http://users.ece.cmu.edu/~koopman/lfsr/16.txt
-	jsr lfsr
-	jsr clearscreen
+	jsr lfsrO
+	inc $d020
 	P_loadi $fe, $8657
-	jsr lfsr
-	jsr clearscreen
+	jsr lfsrA
+	inc $d020
 	P_loadi $fe, $8029
-	jsr lfsr
+	jsr lfsrO
+	inc $d020
+	P_loadi $fe, $855D
+	jsr lfsrA
+	inc $d020
 	nop
 	lda #$37		;Turn on ROMs.
 	sta $01
