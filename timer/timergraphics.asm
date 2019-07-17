3fa3741a733f95f10b4a864fa2a10292ea2346ed
Pararaum <pararaum@gmail.com>
2019-07-17 22:02:06 +0200

    Joystick can be used to change timer value.

--- timer/timergraphics.asm
+++ timer/timergraphics.asm
@@ -1,10 +1,14 @@
 ;;; Produce nice random graphics using the timer.
 
 ;;; Timer is CIA 1, timer B
-timer = $DC06
-timerc = $DC0F
+	timer = $DC06
+	timerc = $DC0F
 ;;; See https://www.c64-wiki.de/wiki/CIA
-CLRSCRCOL = $82
+	CLRSCRCOL = $82
+	TIMESTART = $5555
+
+	;; Zeropage (16bit); timer value
+	ZPTIMEVAL = $15
 
 	* = $0801-2
 
@@ -51,7 +55,9 @@ noov2:	lda p1+2
 	lda #$20
 	sta p1+2		; Write $20 to both high bytes of the pointers.
 	sta p2+2
-skp1:	jmp loop		; This is an endless loop, sorry.
+skp1:
+	jsr checkjoy
+	jmp loop		; This is an endless loop, sorry.
 
 ;;; Clear the text screen.
 clrscr:	.(
@@ -68,9 +74,40 @@ l1	sta $0400,x
 
 ;;; Set timer to $ffff to have the maximum runtime possible.
 inittimer:
-	lda #$ff
-	sta timer
+	lda #<TIMESTART
+	sta timer		; Set timer.
+	sta ZPTIMEVAL		; Set our own counter value.
+	lda #>TIMESTART
 	sta timer+1
-	lda #1
+	sta ZPTIMEVAL+1
+	lda #1			; Start the timer, it will repeat.
 	sta timerc
 	rts
+
+checkjoy:	.(
+	; lda $d012		; Get rasterposition.
+	; cmp #$91		; A random value.
+	; bne out
+	; inc $d020
+        lda #%00000001		; Joystick UP.
+        bit $dc00		; CIA 1, Joystick in Port 2.
+        bne no1			; If joystick pushed up, then pin set to ground.
+        inc ZPTIMEVAL		; Increment the current timer value variable.
+	bne no1
+	inc ZPTIMEVAL+1		; And the high byte on overflow, too.
+no1:	lda #%00000010		; Joystick DOWN.
+        bit $dc00
+        bne no2
+	ldx ZPTIMEVAL
+	dex
+	stx ZPTIMEVAL
+	cpx #$ff
+	bne no2
+	dec ZPTIMEVAL+1
+no2:	lda ZPTIMEVAL
+	sta timer		; Set timer.
+	lda ZPTIMEVAL+1
+	sta timer+1		; Set timer.
+	; dec $d020
+out:	rts
+	.)
