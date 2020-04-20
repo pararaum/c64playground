0281605eb3d5d677fc1fac1d3fa790c05aeca125
Pararaum <pararaum@gmail.com>
2020-04-20 21:55:18 +0200

    Wavy font.

--- wavy_gfx/wavy.s
+++ wavy_gfx/wavy.s
@@ -1,6 +1,10 @@
 
 	.export	_main
 	.import fillcolram
+	.import	animationfont
+	.import	animationfont_end
+	SCREENPTR = $c000
+	CHARGENPTR = $c800
 
 	.code
 _main:
@@ -9,4 +13,18 @@ _main:
 	sta	$d021
 	lda	#3
 	jsr	fillcolram
+	jsr	initialise_screenptr_n_chargen
+	jsr	copy_font
 	rts
+
+copy_font:
+	lda	animationfont
+	ldx	#0
+@l:	lda	animationfont,x
+	sta	CHARGENPTR,x
+	inx
+	cpx	#<(animationfont_end-animationfont)
+	bne	@l
+	rts
+
+	.include "screen_n_char.inc"
