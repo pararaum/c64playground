15b7ee1f50f8403efce47b33a018d7bff8179018
Pararaum <pararaum@gmail.com>
2020-05-03 12:46:43 +0200

    Count encoder.

--- compression/cruncher/src/UniversalCode.hs
+++ compression/cruncher/src/UniversalCode.hs
@@ -18,8 +18,10 @@ module UniversalCode ( w16ToStream
 -- codes. Remember that ones are followed by a zero for length
 -- determination.
 import Data.Bits.Coded
+import Data.Bits.Coding (Coding)
 import Data.Word
-import Data.Bytes.Put (runPutL, flush)
+import Data.Bytes.Put (runPutL, flush, putWord8)
+import Data.Binary.Put (PutM)
 import qualified Data.ByteString.Lazy as BL
 
 -- | Encode Words into Elias Gamma Codes and write them into a ByteString. Remember that putting a zero will cause undesired results.
@@ -29,3 +31,19 @@ w16ToStream xs = pf $ do encodeMany encs
                          flush
   where encs = map Elias xs :: [Gamma Word16 Word16]
         pf = runPutL . runEncode
+
+
+-- | Encode reccuring characters and prefix them with a count
+-- information encoded in Elias Gamma.
+vlcRleComp :: BL.ByteString -> BL.ByteString
+vlcRleComp b = run $ zip bgs ls'
+  where bgs = BL.group b -- Group the ByteString to get recurring bytes.
+        ls = map BL.length bgs -- Get length of elements
+        ls' = map (Elias . fromIntegral) ls :: [Gamma Word16 Word16]
+        run :: [(BL.ByteString, Gamma Word16 Word16)] -> BL.ByteString
+        run xs = runPutL $ runEncode $ mapM_ (uncurry f) xs >> flush
+        f :: BL.ByteString -> Gamma Word16 Word16 -> Coding PutM ()
+        -- TODO: Currently does not work as the stream is flushed after each count information!
+        f x l = do encode l
+                   putWord8 $ BL.head x
+
