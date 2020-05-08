ca214b0a2b2513ec49831d4916f30777f53ad07d
Pararaum <pararaum@gmail.com>
2020-05-08 22:21:00 +0200

    Write crunched file, the pair part.

--- compression/cruncher/src/pairCruncher.hs
+++ compression/cruncher/src/pairCruncher.hs
@@ -12,6 +12,7 @@ is loaded and crunched.
 
 module Main where
 
+import Data.Binary.Put (PutM)
 import Data.Bits.Coded (runEncode)
 import Data.Bits.Coding (Coding, putBit, putBits)
 import Data.Bytes.Put (runPutL, flush)
@@ -60,6 +61,19 @@ encodePairs mlpl s = runPutL $ runEncode $ do mapM_ pB pl' >> flush
         pB (x, y) = putBits b 0 x >> putBits b 0 y -- put the bits
         b = intLog2 $ mlpl + 256
 
+
+encodePairsM :: Int -> RPState -> Coding PutM ()
+encodePairsM mlpl s = mapM_ pB pl'
+  where pl = pairList s
+        pl' = map rePairPair pl
+        pB (x, y) = putBits b 0 x >> putBits b 0 y -- put the bits
+        b = intLog2 $ mlpl + 256
+
+
+writeCrunch :: RPState -> String -> IO ()
+writeCrunch s fn = do BL.writeFile fn d
+  where d = runPutL $ runEncode $ encodePairsM 255 s
+
 -- | Call the cruncher and compress files.
 main :: IO ()
 main = do cpar <- cmdArgs cliparam
@@ -71,7 +85,8 @@ main = do cpar <- cmdArgs cliparam
           print $ pairList compressed
           putStrLn $ printf "$%04X -> $%04X" (BL.length contents) (length $ dataStream compressed)
           putStrLn $ printf "%5d -> %5d" (BL.length contents) (length $ dataStream compressed)
-          putStrLn "@PAIRS@"
+          putStrLn $ printf "%s %d" "@PAIRS@" $ length $ pairList compressed
           BL.putStr $ encodePairs (maxLenPairList cpar) compressed
           putStrLn "Cruncher 7777" 
           BL.putStr $ testBits $ map rePairValue $ dataStream compressed
+          writeCrunch compressed $ files cpar ++ ".crunched"
