17b9176354708ce1014ec1d4df0a2aa0a3fe1bc9
Pararaum <pararaum@gmail.com>
2021-01-01 02:20:51 +0100

    Display image.

---
+++ gfx_n_mzk/display_image.s
@@ -0,0 +1,49 @@
+;;; Simple image viewer.
+
+	.import __bitmap_LOAD__
+	.import __videomatrix_LOAD__
+
+	.segment "bitmap"
+	.incbin	"megapixel-pararaum.koa",2,8000
+
+	.segment "videomatrix"
+	.incbin	"megapixel-pararaum.koa",8002,1000
+
+	.segment "colorram"
+colourram:
+	.incbin	"megapixel-pararaum.koa",9002,1000
+	
+	.rodata
+background:
+	.incbin	"megapixel-pararaum.koa",10002,1
+
+	.code
+	lda	background
+	sta	$d020
+	sta	$d021
+	jsr	initialise_bitmap_and_screenptr
+	lda	#<__bitmap_LOAD__
+	lda	#>__bitmap_LOAD__
+	lda	$d011
+	ora	#$20
+	sta	$d011
+	lda	$d016
+	ora	#$10
+	sta	$d016
+	ldx	#0
+@cl1:	lda	colourram,x
+	sta	$d800,x
+	lda	colourram+$100,x
+	sta	$d800+$100,x
+	lda	colourram+$200,x
+	sta	$d800+$200,x
+	lda	colourram+$300,x
+	sta	$d800+$300,x
+	inx
+	bne	@cl1
+	rts
+
+	SCREENPTR = __videomatrix_LOAD__
+	BITMAPPTR = __bitmap_LOAD__
+	.include	"bitmap_n_screen.inc"
+
