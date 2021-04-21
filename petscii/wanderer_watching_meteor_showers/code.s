d5d100e3162f4dc0e87ac5f317192a664e2e48a9
Pararaum <pararaum@gmail.com>
2021-04-21 21:51:52 +0200

    Added the background and border colours.

--- petscii/wanderer_watching_meteor_showers/code.s
+++ petscii/wanderer_watching_meteor_showers/code.s
@@ -4,6 +4,8 @@
 	.include	"kernal.i"
 	.import		map_data
 	.import		map_colour_data
+	.importzp	SCREEN_BACKGROUND_COLOUR
+	.importzp	SCREEN_BORDER_COLOUR
 	
 	.segment	"STARTUP"
 	.segment	"INIT"
@@ -13,5 +15,9 @@
 	jsr	SCINIT
 	memcpy_down_macro	map_data,$0400,1000
 	memcpy_down_macro	map_colour_data,$d800,1000
-	rts
+	lda	#SCREEN_BACKGROUND_COLOUR
+	sta	$d021
+	lda	#SCREEN_BORDER_COLOUR
+	sta	$d020
+	jmp	*		; Endless loop.
 	
