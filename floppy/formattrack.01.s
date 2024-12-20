6e07763c09e1ca7e053356a8309dda4c7dfbe110
Pararaum <pararaum@gmail.com>
2024-12-20 22:18:45 +0100

    add reference

--- floppy/formattrack.01.s
+++ floppy/formattrack.01.s
@@ -1,4 +1,5 @@
 ;;; Format a track, the track number is used for writing the information onto the disk, a SEEK should be performed first? See R. Immers, G.G. Neufeld, "Inside Commodore DOS", Datamost, 1984, p. 103.
+;;; Formatting code inspired by Gelfand, Felt, Strauch, Krsnik, "Das Anti-Crackerbuch", Data Becker, 1987, p. 248.
 	.include	"t7d/kernal.i"
 	.include	"LAMAlib-macros16.inc"
 
