dc818601931f0ef1345b4bc2357aab54c56747b6
Pararaum <pararaum@gmail.com>
2026-05-29 22:47:26 +0200

    fix pointer invalidation error

--- compression/XipZ/lzp.cc
+++ compression/XipZ/lzp.cc
@@ -3,14 +3,21 @@
 #include <iostream>
 #include <array>
 #include <ranges>
+#include <list>
 
 #define MODELSIZE 8
+#define MAX_RUNLENGTH 255
 
 std::vector<uint8_t> crunch_lzp(const Data &data) {
   std::array<uint8_t,1<<MODELSIZE> model; // This is the buffer and the hash table aka the model.
-  std::vector<uint8_t> output;
+  std::list<uint8_t> output; // A list of output elements, a list is needed for the following trick to work: see mask.
   unsigned long hash = 0;
   unsigned int maskidx = 0;
+  // Pushing the zero and getting a pointer to the value is a nice
+  // trick as we can modify the mask when elements are later on added
+  // without having to worry about splicing this values. But we have
+  // to use a list as a vector invalidates references and therefore
+  // pointers when the capacity is exhausted.
   output.push_back(0); // Add a mask, preinitialised with zeroes.
   uint8_t *mask = &output.back(); // And get a reference to our mask.
   auto hashfun = [&hash](uint8_t x) {
@@ -26,17 +33,16 @@ std::vector<uint8_t> crunch_lzp(const Data &data) {
       maskidx = 0;
     }
   };
-    
 
   model.fill(0);
   for(unsigned long pos = 0; pos < data.size(); ) {
     unsigned int runlength = 0;
 
-    while(runlength < 256) {
+    while(runlength < MAX_RUNLENGTH) {
       if(pos + runlength < data.size()) {
-	if(data[pos + runlength] == model[hash]) { // Still a match?
-	  hashfun(data[pos + runlength]);
-	} else {
+	uint8_t byte = data[pos + runlength];
+	hashfun(byte);
+	if(byte != model[hash]) { // Still a match?
 	  break; // No go to next step.
 	}
       } else {
@@ -48,8 +54,7 @@ std::vector<uint8_t> crunch_lzp(const Data &data) {
       output.push_back(runlength);
       nextmask(1);
       pos += runlength;
-    }
-    if(pos < data.size()) {
+    } else if(pos < data.size()) {
       output.push_back(data[pos]);
       nextmask(0);
       model[hash] = data[pos];
@@ -59,7 +64,10 @@ std::vector<uint8_t> crunch_lzp(const Data &data) {
   }
   output.push_back(0); // Zero length is EOF.
   nextmask(1);
-  return output;
+  // Use C++23 feature to return a vector instead of the list.
+  // Not supported by my compiler version: return output | std::ranges::to<std::vector>();
+  // Use range constructor instead:
+  return std::vector<uint8_t>(output.begin(), output.end());
 }
 
 
